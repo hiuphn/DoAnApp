50a601109df5e937e7339157f76128b359e3ef1c
hiuphn <phamhuynhngochieu468@gmail.com>
2025-01-03 18:59:38 +0700

    test123

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HabitCategoryScreen(),
+      home: BaseScreen(),
       routes: {
         '/profile': (context) => Profile(
           name: 'Phạm Huỳnh Ngọc Hiếu',
